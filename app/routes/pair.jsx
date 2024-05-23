1d4eb1490ac33e08b082920a3ade47faa51b0096
Dixor <Jack.fdrv@gmail.com>
2024-05-23 07:17:02 +0300

    Update logic for donate button.

--- app/routes/pair.jsx
+++ app/routes/pair.jsx
@@ -1,17 +1,35 @@
 import { Response } from "@remix-run/node"; // or cloudflare/deno
+import {
+  getPairByProdId,
+} from '../models/ProductPair.server';
 
 export const loader = async ({ request }) => {
   // Set CORS headers
-  const headers = new Headers();
-  headers.set('Access-Control-Allow-Origin', '*');
-  headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  headers.set('Access-Control-Allow-Credentials', 'true');
-  headers.set('Content-Type', 'application/json'); // Ensure JSON content type
+  const headers = new Headers({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, OPTIONS',
+		'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		'Access-Control-Allow-Credentials': 'true',
+		'Content-Type': 'application/json'
+	});
 
-  // handle "GET" request
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-    headers: headers
-  });
+  const url = new URL(request.url);
+	const productId = url.searchParams.get('prodId');
+
+  if(!productId) {  
+    return new Response(JSON.stringify({ success: false, message: 'productId is required' }), {
+      status: 400,
+      headers: headers
+    });
+  }
+
+  const productPair = await getPairByProdId(productId);
+
+  return new Response(JSON.stringify({ 
+    success: true, 
+    exists: !!productPair, 
+  }), {
+		status: 200,
+		headers: headers
+	});
 };
